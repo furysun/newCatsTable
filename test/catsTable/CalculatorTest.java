70f1a002cb2d239afa90cab8a85684f7588d1ca0
furysun <fury.sun.main@gmail.com>
2018-02-28 12:17:59 +0200

    changed the tests for bdd

--- test/catsTable/CalculatorTest.java
+++ test/catsTable/CalculatorTest.java
@@ -7,10 +7,13 @@ import static org.junit.Assert.*;
 public class CalculatorTest {
     @Test
     public void testPlus() {
+        // given
         Calculator calculator = new Calculator();
 
+        // when
         int result = calculator.plus(1, 4);
 
+        // then
         assertEquals(5, result);
     }
 
